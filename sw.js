ab201334b0044cce37dd81550a0ee57456353c08
Mavis <122183532+yesmavis@users.noreply.github.com>
2026-07-18 19:46:20 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'painel-producao-v1.0.9'; // Subir a versão aqui
+const CACHE_NAME = 'painel-producao-v1.0.10'; // Subir a versão aqui
 const FILES_TO_CACHE = [
   './',
   './index.html',
